73a2f66d65b27098deebd966e914c88fa87e3e5f
valeamoris <ctt@zjmax.com>
2025-10-09 10:04:48 +0800

    refactor(systemcontracts): 移除 Mercury 升级配置
    - 删除了主网和测试网的 Mercury 升级配置
    - 移除了对 Mercury 合约代码的引用
    - 清理了不再需要的导入路径

--- core/systemcontracts/upgrade.go
+++ core/systemcontracts/upgrade.go
@@ -19,7 +19,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/systemcontracts/lorentz"
 	"github.com/ethereum/go-ethereum/core/systemcontracts/luban"
 	"github.com/ethereum/go-ethereum/core/systemcontracts/maxwell"
-	"github.com/ethereum/go-ethereum/core/systemcontracts/mercury"
 	"github.com/ethereum/go-ethereum/core/systemcontracts/mirror"
 	"github.com/ethereum/go-ethereum/core/systemcontracts/moran"
 	"github.com/ethereum/go-ethereum/core/systemcontracts/niels"
@@ -1019,27 +1018,6 @@ func init() {
 				ContractAddr: common.HexToAddress(StakeHubContract),
 				CommitUrl:    "https://github.com/bnb-chain/bsc-genesis-contract/commit/bf3ac733f8aaf93ed88ca0ad2dcddd051166e4e1",
 				Code:         maxwell.RialtoStakeHubContract,
-			},
-		},
-	}
-
-	mercuryUpgrade[mainNet] = &Upgrade{
-		UpgradeName: "mercury",
-		Configs: []*UpgradeConfig{
-			{
-				ContractAddr: common.HexToAddress(ValidatorContract),
-				CommitUrl:    "https://github.com/bnb-chain/bsc-genesis-contract/commit/e0d0c5f0c0c7c5c5c5c5c5c5c5c5c5c5c5c5c5c5c",
-				Code:         mercury.MainnetValidatorContract,
-			},
-		},
-	}
-	mercuryUpgrade[chapelNet] = &Upgrade{
-		UpgradeName: "mercury",
-		Configs: []*UpgradeConfig{
-			{
-				ContractAddr: common.HexToAddress(ValidatorContract),
-				CommitUrl:    "https://github.com/bnb-chain/bsc-genesis-contract/commit/e0d0c5f0c0c7c5c5c5c5c5c5c5c5c5c5c5c5c5c5c",
-				Code:         mercury.ChapelValidatorContract,
 			},
 		},
 	}
